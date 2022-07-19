e20e1fc25bbfe2dd0f5a9a43c1843fad4306c9fb
antoniokallo <antoniokallo@gmail.com>
2022-07-19 12:28:19 -0400

    commit tree 6e5d34718e22e411a2a9590794b354fb77db0e53

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const multiply = (62 * 1)
+let num1 = 62
+let num2 = 1
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = (12 - 8)
+let num3 = 12
+let num4 = 8
+
+const max = Math.max(1, 2, 3, 4, 20);
